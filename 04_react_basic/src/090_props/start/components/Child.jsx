8ffafa3e97cc1a76103a08e01a0419a5ad9828a5
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-06-21 18:11:58 +0900

    props

--- 04_react_basic/src/090_props/start/components/Child.jsx
+++ 04_react_basic/src/090_props/start/components/Child.jsx
@@ -1,14 +1,23 @@
-/* POINT 式と文
-式：何らかの値を返すもの（変数に代入できるもの）
-文：変数宣言、for文、if文、switch文やセミコロンで区切るもの。
-*/
+/* POINT Props の受け取り方
+親コンポーネントで属性値のように記述した各値は、１つのオブジェクトとして子コンポーネントで受け取ることができる。
+ */
+
 
 import "./Child.css";
 
-const Child = () => {
+/* POINT Propsを分割代入で受け取る！ */
+/* POINT Propsのデフォルト値を設定する！ */
+const Child = ({ color: c = 'green', num, fn, bool, obj }) => {
+
+  // console.log(bool);
+
   return (
-    <div className="component">
+    <div className={`component ${c}`}>
       <h3>Hello Component</h3>
+      {/* <h3>{num}</h3>
+      <h3>{fn('Props')}</h3>
+      <h3>{ bool ? 'true' : 'false'}</h3>
+      <h3>{ obj.name + obj.age }</h3> */}
     </div>
   );
 };
