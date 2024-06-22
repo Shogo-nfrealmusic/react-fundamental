a67841cf326f9a130d6a692a6768b44516050391
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-06-22 17:39:30 +0900

    eventHandler

--- 05_state_and_event/src/010_eventlistener/start/Example.jsx
+++ 05_state_and_event/src/010_eventlistener/start/Example.jsx
@@ -1,6 +1,16 @@
 const Example = () => {
+
+  const clickHandler = () => {
+    alert('Click!');
+  }
+  const clickHandler2 = () => {
+    alert('What color is your Bugatti!');
+  }
   return (
-    <></>
+    <>
+      <button onClick={clickHandler}>click!</button>
+      <button onClick={clickHandler2}>click!</button>
+    </>
   );
 };
 
