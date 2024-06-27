9c9d7c3b8f020b64f203d1601af3a603a86f04fb
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-06-27 17:18:38 +0900

    map

--- 06_control_and_form/src/010_list_components/start/Example.jsx
+++ 06_control_and_form/src/010_list_components/start/Example.jsx
@@ -1,13 +1,17 @@
-
 const animals = ["Dog", "Cat", "Rat"];
 
 const Example = () => {
-
+  const helloAnimals = animals.map((animal) => {
+    return <li>Hello {animal}</li>;
+  });
   return (
     <>
       <h3>配列の操作</h3>
       <ul>
-        
+        {helloAnimals}
+        {/* <li>{animals[0]}</li>
+        <li>{animals[1]}</li>
+        <li>{animals[2]}</li> */}
       </ul>
     </>
   );
