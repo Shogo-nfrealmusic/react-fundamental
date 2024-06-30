cb8a7816725e2b9c2184765aaea9a3915079d1f5
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-06-30 18:18:58 +0900

    preventDefault

--- 06_control_and_form/src/130_reminder/start/Components/Form.jsx
+++ 06_control_and_form/src/130_reminder/start/Components/Form.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const Form = ({ createTodo }) => {
   const [enteredTodo, setEnteredTodo] = useState("");
   const addTodo = (e) => {
+    e.preventDefault();
     const newTodo = {
       id: Math.floor(Math.random() * 1e5),
       content: enteredTodo,
@@ -12,12 +13,14 @@ const Form = ({ createTodo }) => {
   };
   return (
     <div>
-      <input
-        type="text"
-        value={enteredTodo}
-        onChange={(e) => setEnteredTodo(e.target.value)}
-      />
-      <button onClick={addTodo}>Add</button>
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          value={enteredTodo}
+          onChange={(e) => setEnteredTodo(e.target.value)}
+        />
+        <button>Add</button>
+      </form>
     </div>
   );
 };
