a55b461237808cc422217773b68f8b0ce49ff686
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-06-30 18:16:00 +0900

    input

--- 06_control_and_form/src/130_reminder/start/Components/Form.jsx
+++ 06_control_and_form/src/130_reminder/start/Components/Form.jsx
@@ -1,5 +1,25 @@
-const Form = () => {
-  return <></>;
+import { useState } from "react";
+
+const Form = ({ createTodo }) => {
+  const [enteredTodo, setEnteredTodo] = useState("");
+  const addTodo = (e) => {
+    const newTodo = {
+      id: Math.floor(Math.random() * 1e5),
+      content: enteredTodo,
+    };
+
+    createTodo(newTodo);
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        value={enteredTodo}
+        onChange={(e) => setEnteredTodo(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+    </div>
+  );
 };
 
 export default Form;
