6acecff69eb3f36089d19050248c18cca140fdd0
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-06-30 19:15:05 +0900

    demerit of inline-style

--- 07_styling_component/src/010_inline_style/start/Example.jsx
+++ 07_styling_component/src/010_inline_style/start/Example.jsx
@@ -4,6 +4,7 @@ const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
 
   const clickHandler = () => setIsSelected((prev) => !prev);
+  const clickHandlere = () => setIsSelected((prev) => !prev);
 
   const style = {
     width: 120,
@@ -16,10 +17,24 @@ const Example = () => {
     margin: "auto",
     background: isSelected ? "pink" : "blue",
   };
+  const style2 = {
+    width: 120,
+    height: 60,
+    display: "block",
+    fontWeight: "bold",
+    "border-radius": 9999,
+    cursor: "pointer",
+    border: "none",
+    margin: "auto",
+    background: isSelected ? "red" : "blue",
+  };
   return (
     <>
       <button onClick={clickHandler} style={style}>
         ボタン
+      </button>
+      <button onClick={clickHandlere} style={style2}>
+        ボタン2
       </button>
       <div>{isSelected && "クリックされました。"}</div>
     </>
