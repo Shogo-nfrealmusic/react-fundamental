f84e1a802b920adc0340f1fa427174a9e864ca0b
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-02 19:48:40 +0900

    chakra_ui

--- 07_styling_component/src/050_chakra_ui/start/components/List.jsx
+++ 07_styling_component/src/050_chakra_ui/start/components/List.jsx
@@ -1,19 +1,29 @@
-const List = ({todos, deleteTodo}) => {
-    const complete = (id) => {
-        deleteTodo(id)
-    }
-    return (
-        <div>
-            {todos.map(todo => {
-                return (
-                    <div key={todo.id}>
-                        <button onClick={() => complete(todo.id)}>完了</button>
-                        <span>{todo.content}</span>
-                    </div>
-                )
-            })}
-        </div>
-    );
-}
+import { StackDivider, VStack } from "@chakra-ui/react";
+
+const List = ({ todos, deleteTodo }) => {
+  const complete = (id) => {
+    deleteTodo(id);
+  };
+  return (
+    <VStack
+      divider={<StackDivider />}
+      color={{ sm: "red.600", md: "blue.600", lg: "green.600", xl: "red.600" }}
+      borderColor="gray.700"
+      borderWidth="3px"
+      borderRadius="3px"
+      p={5}
+      alignItems="start"
+    >
+      {todos.map((todo) => {
+        return (
+          <div key={todo.id}>
+            <button onClick={() => complete(todo.id)}>完了</button>
+            <span>{todo.content}</span>
+          </div>
+        );
+      })}
+    </VStack>
+  );
+};
 
 export default List;
