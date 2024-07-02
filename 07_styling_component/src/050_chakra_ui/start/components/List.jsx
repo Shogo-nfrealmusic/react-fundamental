3b528f3fc770f92e3b31524da2ff24a77b5d874f
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-02 20:02:43 +0900

    css in js with Chakra_ui

--- 07_styling_component/src/050_chakra_ui/start/components/List.jsx
+++ 07_styling_component/src/050_chakra_ui/start/components/List.jsx
@@ -1,4 +1,13 @@
-import { StackDivider, VStack } from "@chakra-ui/react";
+import {
+  VStack,
+  StackDivider,
+  HStack,
+  IconButton,
+  Text,
+} from "@chakra-ui/react";
+
+// POINT react-iconsからアイコンをインポート
+import { VscCheck } from "react-icons/vsc";
 
 const List = ({ todos, deleteTodo }) => {
   const complete = (id) => {
@@ -7,19 +16,29 @@ const List = ({ todos, deleteTodo }) => {
   return (
     <VStack
       divider={<StackDivider />}
-      color={{ sm: "red.600", md: "blue.600", lg: "green.600", xl: "red.600" }}
-      borderColor="gray.700"
-      borderWidth="3px"
+      width="80%"
+      bgColor="white"
+      // color={{ sm: 'red.600', md: 'blue.600', lg: 'green.500', xl: 'red.600' }}
+      borderColor="blackAlpha.100"
+      borderWidth="1px"
       borderRadius="3px"
       p={5}
       alignItems="start"
     >
       {todos.map((todo) => {
         return (
-          <div key={todo.id}>
-            <button onClick={() => complete(todo.id)}>完了</button>
-            <span>{todo.content}</span>
-          </div>
+          <HStack key={todo.id} spacing="5">
+            <IconButton
+              onClick={() => complete(todo.id)}
+              icon={<VscCheck />}
+              isRound
+              bgColor="cyan.100"
+              opacity="0.5"
+            >
+              完了
+            </IconButton>
+            <Text>{todo.content}</Text>
+          </HStack>
         );
       })}
     </VStack>
--- 07_styling_component/src/050_chakra_ui/start/components/Todo.jsx
+++ 07_styling_component/src/050_chakra_ui/start/components/Todo.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+// POINT Chakra UIのホームページから使用したいコンポーネントを見つけてインポート
+import { VStack, Heading } from "@chakra-ui/react";
+
 import List from "./List";
 import Form from "./Form";
 
@@ -33,11 +36,13 @@ const Todo = () => {
   };
 
   return (
-    <>
-      <h2>Reminder</h2>
+    <VStack p="10" spacing="10">
+      <Heading color="blue.200" fontSize="5xl">
+        Reminder
+      </Heading>
       <List todos={todos} deleteTodo={deleteTodo} />
       <Form createTodo={createTodo} />
-    </>
+    </VStack>
   );
 };
 export default Todo;
