51a760f7aa1b3ad882b944219022dbad9553d307
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-02 16:08:24 +0900

    css in js

--- 07_styling_component/src/040_css_in_js/start/Example.jsx
+++ 07_styling_component/src/040_css_in_js/start/Example.jsx
@@ -2,36 +2,69 @@ import { useState } from "react";
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-  margin: auto;
+  margin-inline: auto;
   border-radius: 9999px;
   border: none;
   display: block;
   width: 120px;
   height: 60px;
+  margin: 10px auto;
   font-weight: bold;
   cursor: pointer;
-  color: yellow;
-  background: ${({ isSelected }) => (isSelected ? "pink" : "")};
+  text-align: center;
+  line-height: 60px;
+
+  background-color: ${($isSelected) => ($isSelected ? "pink" : "darkcyan")};
+  transition: all 0.3s ease-out;
+
+  &:hover,
+  :active {
+    opacity: 0.7;
+    transform: scale(1.1);
+  }
+  span {
+    color: purple;
+  }
+  /* POINT メディアクエリ */
+  @media (max-width: 600px) {
+    border-radius: 0;
+  }
+
+  :global {
+    background-color: black;
+  }
 `;
+
+const StyledSubButton = styled(StyledButton)`
+  background-color: ${({ $isSelectedSub }) =>
+    $isSelectedSub ? "crimson" : "coral"};
+`;
+
+const StyledOliveButton = styled(StyledButton)`
+  background-color: olive;
+`;
+
 const Example = () => {
   const [isSelected, setIsSelected] = useState(false);
+  const [isSelectedSub, setIsSelectedSub] = useState(false);
 
-  const clickHandler = () => setIsSelected((prev) => !prev);
+  const onClickHandler = () => setIsSelected(!isSelected);
+  const onClickSubHandler = () => setIsSelectedSub(!isSelectedSub);
 
   return (
     <>
-      <StyledButton isSelected={isSelected} onClick={clickHandler}>
-        button
-      </StyledButton>
-      <button
-        className={`btn ${isSelected ? "selected" : ""}`}
-        onClick={clickHandler}
-      >
+      <StyledButton $isSelected={isSelected} onClick={onClickHandler}>
         ボタン
-      </button>
-      <div style={{ textAlign: "center" }}>
-        {isSelected && "クリックされました。"}
-      </div>
+      </StyledButton>
+      <StyledSubButton
+        $isSelectedSub={isSelectedSub}
+        onClick={onClickSubHandler}
+      >
+        サブボタン
+      </StyledSubButton>
+      <StyledOliveButton>
+        <span>オリーブ</span>
+      </StyledOliveButton>
     </>
   );
 };
