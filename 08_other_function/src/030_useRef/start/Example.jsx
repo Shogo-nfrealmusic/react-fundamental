19595c4a51e6f462a067406671bab9b7ddb2b43b
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-03 18:40:57 +0900

    video with useRef

--- 08_other_function/src/030_useRef/start/Example.jsx
+++ 08_other_function/src/030_useRef/start/Example.jsx
@@ -5,7 +5,7 @@ const Case1 = () => {
   const inputRef = useRef();
   return (
     <div>
-      <h3>ユースケース1</h3>
+      <h3>Case1</h3>
       <input
         type="text"
         ref={inputRef}
@@ -19,10 +19,35 @@ const Case1 = () => {
   );
 };
 
+const Case2 = () => {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef();
+  return (
+    <div>
+      <video style={{ maxWidth: "100%" }} ref={videoRef}>
+        <source src="./sample.mp4"></source>
+      </video>
+      <button
+        onClick={() => {
+          if (playing) {
+            videoRef.current.pause();
+          } else {
+            videoRef.current.play();
+          }
+          setPlaying((prev) => !prev);
+        }}
+      >
+        {playing ? "stop" : "start"}
+      </button>
+    </div>
+  );
+};
+
 const Example = () => {
   return (
     <>
       <Case1 />
+      <Case2 />
     </>
   );
 };
