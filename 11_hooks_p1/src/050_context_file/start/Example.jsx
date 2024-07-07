de4e3c6fd0639ea08e5dd8f2bc03daf6a50e51f2
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-07 18:54:13 +0900

    divide to components with useContext

--- 11_hooks_p1/src/050_context_file/start/Example.jsx
+++ 11_hooks_p1/src/050_context_file/start/Example.jsx
@@ -1,35 +1,16 @@
-import { useState } from "react";
+import { createContext, useState } from "react";
 import "./Example.css";
-
+import Main from "./components/Main";
+import Header from "./components/Header";
+export const ThemeContext = createContext();
 const Example = () => {
   const [theme, setTheme] = useState("light");
 
-  const changeTheme = (e) => setTheme(e.target.value);
-
-  const THEMES = ["light", "dark", "red"];
-
   return (
-    <>
-      <header className={`content-${theme}`}>
-        {THEMES.map((color) => {
-          return (
-            <label>
-              <input
-                type="radio"
-                key={color}
-                value={color}
-                checked={theme === color}
-                onChange={changeTheme}
-              />
-              {color}
-            </label>
-          );
-        })}
-      </header>
-      <main className={`content-${theme}`}>
-        <h1>テーマの切り替え</h1>
-      </main>
-    </>
+    <ThemeContext.Provider value={[theme, setTheme]}>
+      <Header />
+      <Main />
+    </ThemeContext.Provider>
   );
 };
 
--- 11_hooks_p1/src/050_context_file/start/components/Header.jsx
+++ 11_hooks_p1/src/050_context_file/start/components/Header.jsx
@@ -1,5 +1,27 @@
+import { useContext } from "react";
+import { ThemeContext } from "../Example";
 const Header = () => {
-  
+  const [theme, setTheme] = useContext(ThemeContext);
+  const THEMES = ["light", "dark", "red"];
+  const changeTheme = (e) => setTheme(e.target.value);
+  return (
+    <header className={`content-${theme}`}>
+      {THEMES.map((color) => {
+        return (
+          <label>
+            <input
+              type="radio"
+              key={color}
+              value={color}
+              checked={theme === color}
+              onChange={changeTheme}
+            />
+            {color}
+          </label>
+        );
+      })}
+    </header>
+  );
 };
 
 export default Header;
