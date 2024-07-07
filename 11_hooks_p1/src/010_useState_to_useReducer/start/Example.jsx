f061085f96e5e59e3e2f5c91ca7f83926212daee
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-07 17:19:30 +0900

    useReducer

--- 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
+++ 11_hooks_p1/src/010_useState_to_useReducer/start/Example.jsx
@@ -1,7 +1,24 @@
+import { useReducer, useState } from "react";
+
 const Example = () => {
-  
+  const [state, setState] = useState(0);
+  const [rstate, dispatch] = useReducer((prev) => ++prev, 0);
+  const countUp = () => {
+    setState((prev) => ++prev);
+  };
+  const rcountUp = () => {
+    dispatch();
+  };
   return (
     <>
+      <div>
+        <h3>{state}</h3>
+        <button onClick={countUp}>+</button>
+      </div>
+      <div>
+        <h3>{rstate}</h3>
+        <button onClick={rcountUp}>+</button>
+      </div>
     </>
   );
 };
