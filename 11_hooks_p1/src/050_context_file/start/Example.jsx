f74099cfa5cd82dece025d0ca51587d3aad55d7f
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-07 18:46:03 +0900

    refactoring

--- 11_hooks_p1/src/050_context_file/start/Example.jsx
+++ 11_hooks_p1/src/050_context_file/start/Example.jsx
@@ -2,16 +2,29 @@ import { useState } from "react";
 import "./Example.css";
 
 const Example = () => {
-  const [theme, setTheme] = useState('light')
-  
-  const changeTheme = (e) => setTheme(e.target.value)
-  
-  const THEMES = ['light', 'dark', 'red'];
+  const [theme, setTheme] = useState("light");
+
+  const changeTheme = (e) => setTheme(e.target.value);
+
+  const THEMES = ["light", "dark", "red"];
 
   return (
     <>
       <header className={`content-${theme}`}>
-        
+        {THEMES.map((color) => {
+          return (
+            <label>
+              <input
+                type="radio"
+                key={color}
+                value={color}
+                checked={theme === color}
+                onChange={changeTheme}
+              />
+              {color}
+            </label>
+          );
+        })}
       </header>
       <main className={`content-${theme}`}>
         <h1>テーマの切り替え</h1>
