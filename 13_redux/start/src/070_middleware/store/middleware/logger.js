ecebc3e07c9c95ac3bce9523c9084940030893d0
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-18 17:02:05 +0900

    middleware

---
+++ 13_redux/start/src/070_middleware/store/middleware/logger.js
@@ -0,0 +1,10 @@
+const logger = (store) => {
+  return (next) => {
+    return (action) => {
+      console.log(store.getState());
+      next(action);
+      console.log(store.getState());
+    };
+  };
+};
+export default logger;
