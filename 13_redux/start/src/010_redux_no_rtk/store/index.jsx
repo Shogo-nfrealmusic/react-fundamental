ceb0dc652d14f04e7f39ac4e620cf9f524d09244
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-07-18 15:15:34 +0900

    redux

---
+++ 13_redux/start/src/010_redux_no_rtk/store/index.jsx
@@ -0,0 +1,13 @@
+import { createStore } from "redux";
+const initialState = 0;
+const reducer = (prev = initialState, { type, step }) => {
+  switch (type) {
+    case "+":
+      return prev + step;
+    case "-":
+      return prev - step;
+    default:
+      return state;
+  }
+};
+export default createStore(reducer);
