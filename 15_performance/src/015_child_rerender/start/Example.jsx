f12d696cc45265b6bf859ca8c72c26481f70d930
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-07-19 20:47:50 +0900

    parent and child render

--- 15_performance/src/015_child_rerender/start/Example.jsx
+++ 15_performance/src/015_child_rerender/start/Example.jsx
@@ -1,10 +1,41 @@
+import { useState } from "react";
+import Child from "./Child";
+import "./Example.css";
+
 const Example = () => {
+  console.log("Parent render");
+  const [countA, setCountA] = useState(0);
+  const [countB, setCountB] = useState(0);
   return (
-    <>
-    <h3>再レンダリング</h3>
-    <p>親コンポーネントが再レンダリングされると子コンポーネントも再レンダリングされる</p>
-    <p>コンソールを要確認</p>
-    </>
+    <div className="parent">
+      <div>
+        <h3>Parent</h3>
+        <div>
+          <button
+            onClick={() => {
+              setCountA((pre) => pre + 1);
+            }}
+          >
+            buttonA
+          </button>
+          <span>update parent state</span>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              setCountB((pre) => pre + 1);
+            }}
+          >
+            buttonB
+          </button>
+          <span>update child state</span>
+        </div>
+      </div>
+      <div>
+        <p>times:{countA}</p>
+      </div>
+      <Child countB={countB} />
+    </div>
   );
 };
 
