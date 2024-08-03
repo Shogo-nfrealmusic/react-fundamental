744054cddc26cff725bd0f754ed8b255486ceeaa
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2024-08-03 20:29:39 +0900

    useFormState

--- 21_nextjs_app_router/start/src/actions/createItem.js
+++ 21_nextjs_app_router/start/src/actions/createItem.js
@@ -1,7 +1,7 @@
 "use server"
 import { ENDPOINT } from "@/constants";
 
-export async function createItem(formData) {
+export async function createItem(state, formData) {
   const id = formData.get("id");
   const title = formData.get("title");
 
@@ -18,7 +18,7 @@ export async function createItem(formData) {
       body: JSON.stringify({ id, title })
     });
     const data = await res.json();
-    return data;
+    return {msg: `${data.id}:${data.title}の登録が完了しました。`};
   } catch(e) {
     return { msg: '登録に失敗しました。' };
   }
